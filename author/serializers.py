4fb285e94fcb5350fcd33bc1e83ef391baf1eed0
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-06-12 17:19:27 +0300

    feat(serializers): add AuthorSerializer with custom pseudonym field
    - Implement ModelSerializer for Author model

--- author/serializers.py
+++ author/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 
+from author.models import Author
+
 
 class AuthorSerializer(serializers.ModelSerializer):
-    # write your code here
-    pass
+    class Meta:
+        model = Author
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired"
+        ]
+
+    pseudonym = serializers.CharField(
+        max_length=64, required=False, allow_null=True)
